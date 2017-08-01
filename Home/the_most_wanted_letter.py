166ee7575d43253de8f53ba606813ac0ecb75aaf
eddie.sherman@gmail.com <Gr3750n123>
2017-07-31 20:36:38 -0400

    solved the most wanted letter, probably an easier way but i was playing around with dictionaries

--- Home/the_most_wanted_letter.py
+++ Home/the_most_wanted_letter.py
@@ -1,23 +1,28 @@
 import operator
 
+
 def checkio(text):
     c_dict = {}
+    # convert all characters to lowercase
+    text = text.lower()
     for c in text:
         if c.isalpha():
-            c_dict.update({c.lower(): text.count(c)})
+            c_dict.update({c: text.count(c)})
 
     sorted_list = sorted(c_dict.items(), key=operator.itemgetter(1))
-    #print(sorted_dict[-1:][0][1])
-    # Find all keys with values = to the largest.
+
+    # Find all keys with values matching the largest.
+    # sorted_list[-1:][0][1] - gives you the largest value
+    # build a new list match_list with keys = to that value
     match_list = []
     for s in sorted_list:
         if s[1] == sorted_list[-1:][0][1]:
             match_list.append(s[0])
 
+    # sort the keys in alphabetical order
     match_list.sort()
-
+    # return the first key in the sorted list
     return match_list[0]
-    #return 'a'
 
 if __name__ == '__main__':
     #These "asserts" using only for self-checking and not necessary for auto-testing
