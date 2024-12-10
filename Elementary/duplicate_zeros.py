346024bf9ec99a4757fac64905e2e93ae79f55c7
Eddie Sherman <eddie.sherman@gmail.com>
2024-12-09 19:53:07 -0500

    12_9_24_2

---
+++ Elementary/duplicate_zeros.py
@@ -0,0 +1,33 @@
+from collections.abc import Iterable
+
+
+def duplicate_zeros(donuts: list[int]) -> Iterable[int]:
+    result = []
+    for num in donuts:
+        result.append(num)
+        if num == 0:
+            result.append(0)
+    return result
+
+
+print("Example:")
+print(list(duplicate_zeros([1, 0, 2, 3, 0, 4, 5, 0])))
+
+# These "asserts" are used for self-checking
+assert list(duplicate_zeros([1, 0, 2, 3, 0, 4, 5, 0])) == [
+    1,
+    0,
+    0,
+    2,
+    3,
+    0,
+    0,
+    4,
+    5,
+    0,
+    0,
+]
+assert list(duplicate_zeros([0, 0, 0, 0])) == [0, 0, 0, 0, 0, 0, 0, 0]
+assert list(duplicate_zeros([100, 10, 0, 101, 1000])) == [100, 10, 0, 0, 101, 1000]
+
+print("The mission is done! Click 'Check Solution' to earn rewards!")
