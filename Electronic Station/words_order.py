5375f95166f04c26171bc0048e5ec05fe42bb297
Eddie Sherman <eddie.sherman@gmail.com>
2024-12-09 19:31:20 -0500

    12_9_24

---
+++ Electronic Station/words_order.py
@@ -0,0 +1,34 @@
+
+def words_order(text: str, words: list) -> bool:
+    words_in_text = text.split(" ")
+    new_list = []
+    for word in words_in_text:
+        if word in words and word not in new_list:
+            new_list.append(word)
+
+    if new_list == words:
+        return True
+    return False
+
+
+print("Example:")
+print(words_order("hi world im here", ["world", "here"]))
+
+# These "asserts" are used for self-checking
+assert words_order("hi world im here", ["world", "here"]) == True
+assert words_order("hi world im here", ["here", "world"]) == False
+assert words_order("hi world im here", ["world"]) == True
+assert words_order("hi world im here", ["world", "here", "hi"]) == False
+assert words_order("hi world im here", ["world", "im", "here"]) == True
+assert words_order("hi world im here", ["world", "hi", "here"]) == False
+assert words_order("hi world im here", ["world", "world"]) == False
+assert words_order("hi world im here", ["country", "world"]) == False
+assert words_order("hi world im here", ["wo", "rld"]) == False
+assert words_order("", ["world", "here"]) == False
+assert words_order("hi world world im here", ["world", "world"]) == False
+assert (
+    words_order("hi world world im here hi world world im here", ["world", "here"])
+    == True
+)
+
+print("The mission is done! Click 'Check Solution' to earn rewards!")
