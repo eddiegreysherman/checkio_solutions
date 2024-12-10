841f145b2bbe9abbdcca5db5d41de47cf61c01be
Eddie Sherman <eddie.sherman@gmail.com>
2024-12-09 21:55:09 -0500

    completed Days Between

---
+++ Elementary/days_between.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+def days_diff(a: tuple[int, int, int], b: tuple[int, int, int]) -> int:
+    return abs((datetime(*a) - datetime(*b)).days)
+
+
+print("Example:")
+print(days_diff((1982, 4, 19), (1982, 4, 22)))
+
+assert days_diff((1982, 4, 19), (1982, 4, 22)) == 3
+assert days_diff((2014, 1, 1), (2014, 8, 27)) == 238
+assert days_diff((2014, 8, 27), (2014, 1, 1)) == 238
+
+print("The mission is done! Click 'Check Solution' to earn rewards!")
