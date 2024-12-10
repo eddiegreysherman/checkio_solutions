eaf3be56928c4957998824b65f595b4bcd3a454f
eddiegreysherman <eddie.sherman@gmail.com>
2024-12-10 18:07:55 -0500

    bigger_price.py

---
+++ Elementary/bigger_price.py
@@ -0,0 +1,44 @@
+from operator import itemgetter
+
+def bigger_price(limit: int, data: list[dict]) -> list[dict]:
+    """
+    TOP most expensive goods
+    """
+    most_expensive = []
+
+    
+    product = sorted(data, key=itemgetter('price'), reverse=True)
+
+    for i in range(limit):
+        most_expensive.append(product[i])
+    
+    return most_expensive
+
+
+print("Example:")
+print(
+    bigger_price(
+        2,
+        [
+            {"name": "bread", "price": 100},
+            {"name": "wine", "price": 138},
+            {"name": "meat", "price": 15},
+            {"name": "water", "price": 1},
+        ],
+    )
+)
+
+assert bigger_price(
+    2,
+    [
+        {"name": "bread", "price": 100},
+        {"name": "wine", "price": 138},
+        {"name": "meat", "price": 15},
+        {"name": "water", "price": 1},
+    ],
+) == [{"name": "wine", "price": 138}, {"name": "bread", "price": 100}]
+assert bigger_price(
+    1, [{"name": "pen", "price": 5}, {"name": "whiteboard", "price": 170}]
+) == [{"name": "whiteboard", "price": 170}]
+
+print("The mission is done! Click 'Check Solution' to earn rewards!")
