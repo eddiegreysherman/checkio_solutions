68c7a3e6e8a1274906eebe2198bc63d00c2a47bc
eddiegreysherman <eddie.sherman@gmail.com>
2024-12-12 09:48:12 -0500

    finished between_markers

---
+++ Elementary/between_markers.py
@@ -0,0 +1,34 @@
+def between_markers(text: str, begin: str, end: str) -> str:
+    """
+    returns substring between two given markers
+    """
+    if begin in text and end not in text:
+        return(text.split(begin)[1])
+    
+    elif end in text and begin not in text:
+        return(text.split(end)[0])
+    
+    elif begin and end in text:
+        return(text.split(begin)[1].split(end)[0])
+    
+    elif begin and end not in text:
+        return text
+    
+    else:
+        return ""
+
+
+print("Example:")
+print(between_markers("What is >apple<", ">", "<"))
+
+assert between_markers("What is >apple<", ">", "<") == "apple"
+assert (
+    between_markers("<head><title>My new site</title></head>", "<title>", "</title>")
+    == "My new site"
+)
+assert between_markers("No[/b] hi", "[b]", "[/b]") == "No"
+assert between_markers("No [b]hi", "[b]", "[/b]") == "hi"
+assert between_markers("No hi", "[b]", "[/b]") == "No hi"
+assert between_markers("No <hi>", ">", "<") == ""
+
+print("The mission is done! Click 'Check Solution' to earn rewards!")
