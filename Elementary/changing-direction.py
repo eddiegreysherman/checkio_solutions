bb97882192779b6f6c9bdf6a2a7f19c2006316c4
Eddie Sherman <eddie.sherman@gmail.com>
2024-12-14 12:19:01 -0500

    completed changing directions

--- Elementary/changing-direction.py
+++ Elementary/changing-direction.py
@@ -1,29 +1,27 @@
 def changing_direction(elements: list[int]) -> int:
     changes = 0
-    direction = "asc"
-    for i in range(0, len(elements) - 1):
-        if i == 0:
-            continue
-        if (elements[i] >= elements[i - 1]):
-            # no change was made, going up
-            # if prior changes we need to check
-            if changes != 0:
-                if (elements[i] > elements[i - 1]) or (elements[i] < elements[i - 1]):
-                    changes += 1
-            continue
-        if (elements[i] < elements[i - 1]):
-            #there was a change so count it
-            changes += 1
+    direction = None
+
+    for i in range(1, len(elements)):
+        # Ascending
+        if elements[i] > elements[i - 1]:
+            if direction == "desc":
+                changes += 1
+            direction = "asc"
+        elif elements[i] < elements[i - 1]:
+            if direction == "asc":
+                changes += 1
+            direction = "desc"
     return changes
 
 
 print("Example:")
-print(changing_direction([1, 2, 3, 2, 1]))
+print(changing_direction([1, 2, 2, 1, 2, 2]))
 
 # These "asserts" are used for self-checking
-#assert changing_direction([1, 2, 3, 4, 5]) == 0
-#assert changing_direction([1, 2, 3, 2, 1]) == 1
-#assert changing_direction([1, 2, 2, 1, 2, 2]) == 2
-#assert changing_direction([1, 2, 2, 1, 2, 1, 2]) == 4
+assert changing_direction([1, 2, 3, 4, 5]) == 0
+assert changing_direction([1, 2, 3, 2, 1]) == 1
+assert changing_direction([1, 2, 2, 1, 2, 2]) == 2
+assert changing_direction([1, 2, 2, 1, 2, 1, 2]) == 4
 
 print("The mission is done! Click 'Check Solution' to earn rewards!")
